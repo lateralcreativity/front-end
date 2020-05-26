e8b19e13a64c07656f029c1f25b483b132d41270
lateralcreativity <lateralcreativity@outlook.com>
2020-05-26 12:14:38 -0500

    Changed copyright href and name, sign up form changed to respresent data structure provided to us, validation errors updated as well, icon and name updated in nav component

--- use-my-tech-stuff-app/src/validation/formSchema.js
+++ use-my-tech-stuff-app/src/validation/formSchema.js
@@ -1,17 +1,16 @@
 import * as yup from 'yup';
 
 const formSchema = yup.object().shape({
-    firstName: yup.string()
+    username: yup.string()
     .trim()
-    .required('First name is a required field'),
-    lastName: yup.string()
-    .trim()
-    .required('Last name is a required field'),
+    .required('Username is a required field.'),
     email: yup.string()
     .required('Must be a valid email address.')
     .email('Must be a valid email address.'),
     password: yup.string()
-    .required('Password is a required field.')
+    .required('Password is a required field.'),
+    type: yup.string()
+    .required('User type is a required field.')
   })
 
   export default formSchema

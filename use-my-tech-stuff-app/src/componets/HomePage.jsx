dfdb69476744a8af9612f25b725514aceaa6ccb7
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-05-26 12:30:39 -0700

    added favicon and tab title

--- use-my-tech-stuff-app/src/componets/HomePage.jsx
+++ use-my-tech-stuff-app/src/componets/HomePage.jsx
@@ -8,6 +8,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia';
+import profilePlaceholderImage from '../images/profile-pic-placeholder.png';
+
 
 import NavBar from './NavBar'
 import ListItem from './ListItem'
@@ -34,6 +37,7 @@ const HomePage = () => {
           heroContent: {
                backgroundColor: theme.palette.background.paper,
                padding: theme.spacing(8, 0, 6),
+               margin: '4%',
           },
           heroButtons: {
                marginTop: theme.spacing(4),
@@ -51,10 +55,17 @@ const HomePage = () => {
           card: {
                height: '100%',
                display: 'flex',
-               flexDirection: 'column',
                margin: '2%',
                alignItems: 'center',
           },
+          cardMedia: {
+               maxWidth: '300px',
+               height: '280px',
+               border: '1px',
+               borderRadius: '5px',
+               boxShadow: '0 4px 12px 0px rgba(0,0,0,.25)',
+               margin: '2%'
+             },
      }));
 
      const classes = useStyles();
@@ -74,27 +85,13 @@ const HomePage = () => {
                     <div className={classes.heroContent}>
                          <Container maxWidth="sm">
                               <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                                   Home Page
-            </Typography>
+                                   Home
+                              </Typography>
                               <Typography variant="h5" align="center" color="textSecondary" paragraph>
                                    Something short and leading about the collection below—its contents, the creator, etc.
                                    Make it short and sweet, but not too short so folks don&apos;t simply skip over it
                                    entirely.
-            </Typography>
-                              <div className={classes.heroButtons}>
-                                   <Grid container spacing={2} justify="center">
-                                        <Grid item>
-                                             <Button variant="contained" color="primary">
-                                                  Main call to action
-                  </Button>
-                                        </Grid>
-                                        <Grid item>
-                                             <Button variant="outlined" color="primary">
-                                                  Secondary action
-                  </Button>
-                                        </Grid>
-                                   </Grid>
-                              </div>
+                              </Typography>
                          </Container>
                     </div>
                     {/* End hero unit */}
@@ -103,8 +100,12 @@ const HomePage = () => {
                     <Container className={classes.cardGrid} maxWidth="md">
 
                          <Card className={classes.card}>
-                              Link to Profile
-               <Button
+                              <img 
+                                   className={classes.cardMedia}
+                                   src='https://www.kindpng.com/picc/m/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png'
+                                   title="Profile Pic Placeholder"
+                              />
+                              <Button
                                    type='submit'
                                    variant='contained'
                                    color='secondary'
@@ -115,12 +116,16 @@ const HomePage = () => {
                               >
                                    <span id='profile'>
                                         Profile
-                    </span>
+                                   </span>
                               </Button>
                          </Card>
                          <Card className={classes.card}>
-                              Link to 'Rent Your Tech'
-               <Button
+                              <img 
+                                   className={classes.cardMedia}
+                                   src='https://images.unsplash.com/photo-1577375774296-1480089cb555?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'
+                                   title="Profile Pic Placeholder"
+                              />
+                              <Button
                                    variant='contained'
                                    color='secondary'
                                    onClick={buttonLinkHandler}
@@ -131,8 +136,12 @@ const HomePage = () => {
           </span></Button>
                          </Card>
                          <Card className={classes.card}>
-                              Link to 'RentalsList'
-               <Button
+                              <img
+                                   className={classes.cardMedia}
+                                   src='https://images.unsplash.com/photo-1546624156-249875babb27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=799&q=80'
+                                   title='computer desk setup'
+                                   />
+                              <Button
                                    size='large'
                                    variant='contained'
                                    color='secondary'

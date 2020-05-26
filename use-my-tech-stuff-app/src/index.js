36246a6cc3f3089008feba41274fc765d116670c
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-05-25 18:06:15 -0700

    set up redux store, reducer, and actions - added functionality to PrivateRoute and axiosWithAuth

--- use-my-tech-stuff-app/src/index.js
+++ use-my-tech-stuff-app/src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//importing redux tools
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+import { reducer } from './store/reducers'
+
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store} >
+      <App />
+    </Provider>
+  </React.StrictMode>
+
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ use-my-tech-stuff-app/src/store/reducers.js
@@ -0,0 +1,14 @@
+//import actions
+import {} from '../store/actions'
+
+
+//initialState
+const initialState = {
+}
+
+export const reducer = (state = initialState, action) => {
+     switch(action.type) {
+          default:
+               return state
+     }
+}
